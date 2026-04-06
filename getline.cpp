c3106b4eea8e7eb5c241b834ed62e17fb6af6124
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-04-06 18:50:53 +0700

    mengisi data 2

--- getline.cpp
+++ getline.cpp
@@ -6,3 +6,22 @@ struct detailAlamat
     string kota;
 };
 struct Orang 
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
+int main () {
+    Orang mhs;
+    // Mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : " ;
+    getline(cin, mhs.nama);
+    cout << "desa : " ;
+    getline(cin, mhs.alamat.desa);
+    cout << "kota : " ;
+    getline(cin, mhs.alamat.kota);
+    cout << "Umur : " ;
+    cin >> mhs.umur;
+    cout << endl;
