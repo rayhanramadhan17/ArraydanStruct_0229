09d0de69e75a490362178aa6e1711e143140e4f0
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-04-06 18:51:24 +0700

    menampilkan hasil getline

--- getline.cpp
+++ getline.cpp
@@ -25,3 +25,11 @@ int main () {
     cout << "Umur : " ;
     cin >> mhs.umur;
     cout << endl;
+    // Menampilkan data
+    cout << "Menampilkan Data" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "desa : " << mhs.alamat.desa << endl;
+    cout << "kota : " << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
+
+}
