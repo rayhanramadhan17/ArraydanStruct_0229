caaf600715bf825835ab4ca1fe8d12ee76ee4433
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-04-06 19:11:56 +0700

    mengisi data 2

--- ArrayStruct.cpp
+++ ArrayStruct.cpp
@@ -15,3 +15,23 @@ struct Orang
     int umur;
 };
 
+int main() {
+    Orang mhs[3];
+
+    for (int i = 0; i <= 2; i++)
+    {
+
+    // mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    getline(cin ,  mhs[i].nama);
+    cout << "Alamat : ";
+    getline(cin , mhs[i].alamat.desa);
+    cout << "Alamat : ";
+    getline(cin , mhs[i].alamat.kota);
+    cout << "Umur : ";
+    cin >> mhs[i].umur;
+    cout << endl;
+
+    }
+
