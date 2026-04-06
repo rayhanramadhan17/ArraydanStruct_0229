5a409d6f6d047baa0b124c28b4252fc10df5b50f
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-04-06 18:28:56 +0700

    mengisi data

--- StructNew..cpp
+++ StructNew..cpp
@@ -8,3 +8,15 @@ struct Orang
     int umur;
 };
 
+int main() {
+    Orang mhs;
+    // Mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Alamat : ";
+    cin >> mhs.alamat;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+   
