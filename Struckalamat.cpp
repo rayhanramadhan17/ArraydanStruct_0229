83c626c821ed22646d187b557031af7fed47f097
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-04-06 18:36:05 +0700

    mengisi data

--- Struckalamat.cpp
+++ Struckalamat.cpp
@@ -13,3 +13,23 @@ struct Orang
     DetailAlamat alamat;
     int umur;
 };
+int main() {
+    Orang mhs;
+    // mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Alamat : ";
+    cin >> mhs.alamat.desa;
+    cout << "Alamat : ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+    // tampilkan
+    cout << "Menampilkan Data" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat.desa << ", " << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
+
